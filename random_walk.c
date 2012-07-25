cc32f998dd43c5e236fc860a87e49966e7abb81e
cforth <cforth@163.com>
2012-07-25 14:35:09 +0800

    使用图形显示醉汉10步的路线图

--- random_walk.c
+++ random_walk.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
-
+#define MAX 20
 int main( void )
 {
-	int i;
-	int sum_x = 0;
-	int sum_y = 0;
+	int i, j, point_x, point_y, old_x, old_y;
+	char step = '0';
+	char plot[MAX][MAX*2];
+	point_x = point_y = old_x = old_y = 0;
 	srand((unsigned)time(0));
 	
-	for( i = 0; i < 10000; i++) {
+	for(i = 0; i < MAX; i++) 
+		for(j = 0; j < MAX*2; j++)
+			plot[i][j] = ' ';
+
+	for( i = 0; i < MAX/2; i++) {
 		if( roll(2) )
-			sum_x += (roll(3) - 1);
+			point_x += (roll(3) - 1);
 		else 
-			sum_y += (roll(3) - 1);
-		printf("Drunk walked to %5d %5d\n", sum_x, sum_y);
+			point_y += (roll(3) - 1);
+		if(point_x != old_x || point_y != old_y)
+			step++;
+		old_x = point_x;
+		old_y = point_y;
+		plot[point_y + MAX/2][point_x*2 + MAX] = step;
 	}
+	
+	for(i = 0; i < MAX*2 + 2; i++)
+		printf("-");
+	printf("\n");
+	for(i = 0; i < MAX; i++) {
+		printf("|");
+		for(j = 0; j < MAX*2; j++)
+			printf("%c",plot[i][j]);
+		printf("|\n");
+	}
+	for(i = 0; i < MAX*2 + 2; i++)
+		printf("-");
+	printf("\n");
 	return 0;
 }
 

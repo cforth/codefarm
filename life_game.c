e38d2f648c15a323ab781751618a63225c8bb35f
cforth <cforth@163.com>
2012-07-27 12:57:10 +0800

    MAX为空间最大边长，MAX_STEP为演化的次数

--- life_game.c
+++ life_game.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 #define MAX 20
-#define MAX_STEP 10000
+#define MAX_STEP 100
 typedef struct {
 	int x;
 	int y;
 } point;
 
 int roll( int max_num );
-point make_point( point snake, int n );
+point make_point( point spore, int n );
 int one_step( void );
 
 int main( void )
 {
 	int i, j, step, old_x, old_y, c;
 	char space[MAX][MAX*2];
-	point snake_a = {roll(20), roll(20)};
-	point snake_b = {roll(20), roll(20)};
-	point snake_c = {roll(20), roll(20)};
-	point snake_d = {roll(20), roll(20)};
+	point spore_a = {roll(MAX), roll(MAX)};
+	point spore_b = {roll(MAX), roll(MAX)};
+	point spore_c = {roll(MAX), roll(MAX)};
+	point spore_d = {roll(MAX), roll(MAX)};
 	srand((unsigned)time(0));
 
 	for(i = 0; i < MAX; i++)
@@ -28,44 +28,44 @@ int main( void )
 	
 	for(step = 0; step < MAX_STEP; step++) {
 
-		old_x = snake_a.x;
-		old_y = snake_a.y;
-		snake_a = make_point( snake_a, one_step() ); 
-		if ( (c = space[snake_a.x][(snake_a.y)*2]) == ' ' || c == '*' )
-			space[snake_a.x][(snake_a.y)*2]= '*';
+		old_x = spore_a.x;
+		old_y = spore_a.y;
+		spore_a = make_point( spore_a, one_step() ); 
+		if ( (c = space[spore_a.x][(spore_a.y)*2]) == ' ' || c == '*' )
+			space[spore_a.x][(spore_a.y)*2]= '*';
 		else {
-			snake_a.x = old_x;
-			snake_a.y = old_y;
+			spore_a.x = old_x;
+			spore_a.y = old_y;
 		}
 
-		old_x = snake_b.x;
-		old_y = snake_b.y;
-		snake_b = make_point( snake_b, one_step() ); 
-		if ( (c = space[snake_b.x][(snake_b.y)*2]) == ' ' || c == '#' )
-			space[snake_b.x][(snake_b.y)*2]= '#';
+		old_x = spore_b.x;
+		old_y = spore_b.y;
+		spore_b = make_point( spore_b, one_step() ); 
+		if ( (c = space[spore_b.x][(spore_b.y)*2]) == ' ' || c == '#' )
+			space[spore_b.x][(spore_b.y)*2]= '#';
 		else {
-			snake_b.x = old_x;
-			snake_b.y = old_y;
+			spore_b.x = old_x;
+			spore_b.y = old_y;
 		}
 
-		old_x = snake_c.x;
-		old_y = snake_c.y;
-		snake_c = make_point( snake_c, one_step() ); 
-		if ( (c = space[snake_c.x][(snake_c.y)*2]) == ' ' || c == '@' )
-			space[snake_c.x][(snake_c.y)*2]= '@';
+		old_x = spore_c.x;
+		old_y = spore_c.y;
+		spore_c = make_point( spore_c, one_step() ); 
+		if ( (c = space[spore_c.x][(spore_c.y)*2]) == ' ' || c == '@' )
+			space[spore_c.x][(spore_c.y)*2]= '@';
 		else {
-			snake_c.x = old_x;
-			snake_c.y = old_y;
+			spore_c.x = old_x;
+			spore_c.y = old_y;
 		}
 
-		old_x = snake_d.x;
-		old_y = snake_d.y;
-		snake_d = make_point( snake_d, one_step() ); 
-		if ( (c = space[snake_d.x][(snake_d.y)*2]) == ' ' || c == '$' )
-			space[snake_d.x][(snake_d.y)*2]= '$';
+		old_x = spore_d.x;
+		old_y = spore_d.y;
+		spore_d = make_point( spore_d, one_step() ); 
+		if ( (c = space[spore_d.x][(spore_d.y)*2]) == ' ' || c == '$' )
+			space[spore_d.x][(spore_d.y)*2]= '$';
 		else {
-			snake_d.x = old_x;
-			snake_d.y = old_y;
+			spore_d.x = old_x;
+			spore_d.y = old_y;
 		}
 
 	}
@@ -93,21 +93,21 @@ int roll(int max_num)
 	return rand()%max_num;
 }
 
-point make_point( point snake, int n )
+point make_point( point spore, int n )
 {
-	int temp_x = snake.x;
-	int temp_y = snake.y;
+	int temp_x = spore.x;
+	int temp_y = spore.y;
 
 	if(roll(2))
-		snake.x += n;
+		spore.x += n;
 	else
-		snake.y += n;
-	if((snake.x < 0 || snake.x >= MAX) || (snake.y < 0 || snake.y >= MAX)) {
-	      snake.x = temp_x;
-	      snake.y = temp_y;
+		spore.y += n;
+	if((spore.x < 0 || spore.x >= MAX) || (spore.y < 0 || spore.y >= MAX)) {
+	      spore.x = temp_x;
+	      spore.y = temp_y;
 	}
 
-	return snake;
+	return spore;
 }
 
 int one_step( void )

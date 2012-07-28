5cd6df62e0ac55e361a0427bd2f36d4f6de141c3
xiaohao@cforth <cforth@163.com>
2012-07-28 12:55:03 +0800

    is_full函数检测space数组是否已填满

--- color_4.c
+++ color_4.c
@@ -10,11 +10,12 @@ typedef struct {
 int roll( int max_num );
 point make_point( point spore, int n );
 int one_step( void );
+int is_full( char (*s)[MAX] );
 
 int main( void )
 {
-	int i, j, old_x, old_y, c, step, is_full;
-	char space[MAX][MAX*2];
+	int i, j, old_x, old_y, c, step;
+	char space[MAX][MAX];
 	point spore_a = {roll(MAX), roll(MAX)};
 	point spore_b = {roll(MAX), roll(MAX)};
 	point spore_c = {roll(MAX), roll(MAX)};
@@ -22,17 +23,17 @@ int main( void )
 	srand((unsigned)time(0));
 
 	for(i = 0; i < MAX; i++)
-	      for(j = 0; j < MAX*2; j++)
+	      for(j = 0; j < MAX; j++)
 		    space[i][j] = ' ';
 	
-	for(step = 0, is_full = 0; is_full == 0; step++) {
+	for(step = 0; is_full( &space[0] ) == 0; step++) {
 
 		/*spore_a执行一步*/
 		old_x = spore_a.x;
 		old_y = spore_a.y;
 		spore_a = make_point( spore_a, one_step() ); 
-		if ( (c = space[spore_a.x][(spore_a.y)*2]) == ' ' || c == '*' )
-			space[spore_a.x][(spore_a.y)*2]= '*';
+		if ( (c = space[spore_a.x][spore_a.y]) == ' ' || c == '*' )
+			space[spore_a.x][spore_a.y]= '*';
 		else {
 			spore_a.x = old_x;
 			spore_a.y = old_y;
@@ -42,8 +43,8 @@ int main( void )
 		old_x = spore_b.x;
 		old_y = spore_b.y;
 		spore_b = make_point( spore_b, one_step() ); 
-		if ( (c = space[spore_b.x][(spore_b.y)*2]) == ' ' || c == '#' )
-			space[spore_b.x][(spore_b.y)*2]= '#';
+		if ( (c = space[spore_b.x][spore_b.y]) == ' ' || c == '#' )
+			space[spore_b.x][spore_b.y]= '#';
 		else {
 			spore_b.x = old_x;
 			spore_b.y = old_y;
@@ -53,8 +54,8 @@ int main( void )
 		old_x = spore_c.x;
 		old_y = spore_c.y;
 		spore_c = make_point( spore_c, one_step() ); 
-		if ( (c = space[spore_c.x][(spore_c.y)*2]) == ' ' || c == '@' )
-			space[spore_c.x][(spore_c.y)*2]= '@';
+		if ( (c = space[spore_c.x][spore_c.y]) == ' ' || c == '@' )
+			space[spore_c.x][spore_c.y]= '@';
 		else {
 			spore_c.x = old_x;
 			spore_c.y = old_y;
@@ -64,24 +65,12 @@ int main( void )
 		old_x = spore_d.x;
 		old_y = spore_d.y;
 		spore_d = make_point( spore_d, one_step() ); 
-		if ( (c = space[spore_d.x][(spore_d.y)*2]) == ' ' || c == '$' )
-			space[spore_d.x][(spore_d.y)*2]= '$';
+		if ( (c = space[spore_d.x][spore_d.y]) == ' ' || c == '$' )
+			space[spore_d.x][spore_d.y]= '$';
 		else {
 			spore_d.x = old_x;
 			spore_d.y = old_y;
 		}
-
-		/*测试space数组中是否有空位，有空位返回0，无空位返回1*/
-		for(i = 0, is_full = 1; i < MAX; i++) { 
-	    	for(j = 0; j < MAX*2; j+=2)
-		    	if (space[i][j] == ' ') {
-					is_full = 0;
-					break;
-				}
-			if(is_full == 0)
-				break;
-		}
-
 	}
 
 	/*打印整个space数组，显示到屏幕上*/
@@ -91,8 +80,8 @@ int main( void )
 
 	for(i = 0; i < MAX; i++) {
 		printf("|");
-		for(j = 0; j < MAX*2; j++)
-		      printf("%c", space[i][j]);
+		for(j = 0; j < MAX; j++)
+		      printf("%c ", space[i][j]);
 		printf("|\n");
 	}
 
@@ -132,4 +121,16 @@ point make_point( point spore, int n )
 int one_step( void )
 {
 	return (roll(3) - 1);
+}
+
+int is_full( char (*s)[MAX] )
+{
+	int i, j;
+	/*测试space数组中是否有空位，有空位返回0，无空位返回1*/
+	for(i = 0; i < MAX; i++, s++) {
+		for(j = 0; j < MAX; j++) 
+			if ((*s)[j] == ' ')
+				return 0;
+	}
+	return 1;
 }

bb1ef98ed22c90396d28a82ce3d9568d469757da
cforth <cforth@163.com>
2012-08-02 13:49:24 +0800

    重构了数字推盘游戏自动版本，运行速度大大提升

--- squared_sort.c
+++ squared_sort.c
@@ -1,91 +1,133 @@
+/*
+** 数字推盘游戏
+** 将矩阵中的数字按照从小到大排列。
+** 自动运行版本。
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+
+/* MAX为矩阵边长。
+** NUM为矩阵中的数字标号个数，必须是最大个数减1。
+** CLASS为起始数字标号。
+*/
 #define MAX 3
 #define NUM 8
-#define MAX_STEP 9999999 
 #define CLASS '1'
+#define MAX_STEP 9999999
+
+/* 定义point类型结构体。
+** 用来保存矩阵中单元的横坐标和纵坐标。
+*/
 typedef struct {
 	int x;
 	int y;
 } point;
 
+/*
+** roll
+** 返回 0到max_num-1之间的随机整数。
+** 用来确定数字标号在矩阵中的起始位置。
+*/
 int roll( int max_num );
+
+/*
+** init_point
+** 初始化point，返回point类型结构体。
+** 用来给结构体赋值, 初始化矩阵的起始状态。
+*/
 point init_point(int x, int y);
-point make_point( point spore, int n );
-int one_step( void );
-void printf_space( char (*s)[MAX] );
+
+/*
+** printf_matrix
+** 打印matrix二维矩阵，显示到屏幕上。
+** 用来显示每一次数字标号移动后的矩阵。
+*/
+void printf_matrix( char (*s)[MAX] );
+
+/*
+** is_win
+** 矩阵中的数字标号按顺序排列时，返回1， 否则返回0。
+** 用来检测矩阵中的数字标号是否已经排列完成。
+*/
 int is_win( char(*s)[MAX] );
+
+/*
+** find_point
+** 寻找matrix矩阵中的空格位置，将坐标存入point类型的结构体返回。
+** 用来找出指定矩阵中的空位，确定能够移动的数字标号的位置。
+*/
+point find_point( char(*s)[MAX] );
+
 
 int main( void )
 {
-	int i, old_x, old_y, c;
+	int i, c;
 	unsigned long step;
-	char space[MAX][MAX];
+	char matrix[MAX][MAX];
 	char class;
 
-	/*初始化随机数发生器、space矩阵、spoce结构体数组*/
+	/*初始化随机数发生器、matrix矩阵、spoce结构体数组*/
 	srand((unsigned)time(0));
 	point spore[NUM];
+	point site;
 	memset( spore, 0, sizeof(spore) );
-	memset( space, ' ', sizeof(space) );
+	memset( matrix, ' ', sizeof(matrix) );
 	
-	/*初始化最初随机乱序的矩阵space*/
+	/*初始化数字标号随机乱序的矩阵matrix*/
 	printf("I'm initializing...\n");
 
-	for(i = 0, class = CLASS; i < NUM; i++, class++) {
+	for(i = 0, class = CLASS; i < NUM; i++) {
 		spore[i] = init_point(roll(MAX), roll(MAX));
 		
-		while(space[spore[i].x][spore[i].y] != ' ')
+		while(matrix[spore[i].x][spore[i].y] != ' ')
 			spore[i] = init_point(roll(MAX), roll(MAX));
 		
-		space[spore[i].x][spore[i].y]= class;
+		matrix[spore[i].x][spore[i].y]= class;
+		
+		if(class == '9')
+			class = 'A';
+		else
+			class++;
 	}
 
-	printf_space( &space[0] );
+	printf_matrix( &matrix[0] );
 	printf("I'm working...\n");
 
-
-	/*通过随机移动来对space矩阵中的标号排序。
-	 *每个标号每一次随机移动一格，遇到空间边框或其他标号则该次移动无效。
-	 *排序成功后，打印出标号经排序后的矩阵，游戏成功。
-	 *执行了MAX_STEP步数后仍未完成，则游戏失败						*/
-	for(step = 0; is_win(&space[0]) != 1; step++) {
-		class = CLASS;
-
-		for(i = 0; i < NUM; i++) {
-			old_x = spore[i].x;
-			old_y = spore[i].y;
-
-			spore[i] = make_point( spore[i], one_step() );
-
-			if ( (c = space[spore[i].x][spore[i].y]) == ' ') {
-				space[spore[i].x][spore[i].y]= class;
-				space[old_x][old_y] = ' ';
-			}
-			else if ( c != ' ' && c != class ) {
-				spore[i].x = old_x;
-				spore[i].y = old_y;
-			}
-
-			class++;
-		}
-
-		if(step > MAX_STEP) {
-			printf_space( &space[0] );
-			printf("I have failed.\n");
-			printf("Used %lu steps.\n", step);
-			exit(0);
-		}	
-
+	/*随机移动数字标号，若游戏胜利或者步数超过一千万步则退出，并打印出矩阵*/
+	for(step = 0; step <= MAX_STEP && is_win(&matrix[0]) != 1; step++) {
+		c = roll(4);
+		site = find_point( &matrix[0] );
+		switch(c) {
+		case 0:
+			if((site.y + 1) < MAX) {
+				matrix[site.x][site.y] = matrix[site.x][site.y + 1];
+				matrix[site.x][site.y + 1] = ' ';
+			} break;
+		case 1:
+			if((site.y - 1) >= 0) {
+				matrix[site.x][site.y] = matrix[site.x][site.y - 1];
+				matrix[site.x][site.y - 1] = ' ';
+			} break;
+		case 2:
+			if((site.x - 1) >= 0) {
+				matrix[site.x][site.y] = matrix[site.x - 1][site.y];
+				matrix[site.x - 1][site.y] = ' ';
+			} break;
+		case 3:
+			if((site.x + 1) < MAX) {
+				matrix[site.x][site.y] = matrix[site.x + 1][site.y];
+				matrix[site.x + 1][site.y] = ' ';
+			} break;
+		default:
+			break;
+		}		
 	}
 
-	/*打印出经过排序的space矩阵，显示到屏幕上*/
-	printf_space( &space[0] );
+	printf_matrix( &matrix[0] );
 
 	/*打印出总共执行的步数step*/
-	printf("I win!\n");
 	printf("Used %lu steps.\n", step);
 	return 0;
 }
@@ -93,19 +135,17 @@ int main( void )
 
 /*
 ** roll
-** 返回 0到max_num-1之间的随机整数。
 */
-int roll(int max_num)
+int roll( int max_num )
 {
-	return rand()%max_num;
+	return rand() % max_num;
 }
 
 
 /*
 ** init_point
-** 初始化ponit，返回point类型结构体。
 */
-point init_point(int x, int y)
+point init_point( int x, int y )
 {
 	point temp;
 	temp.x = x;
@@ -115,42 +155,9 @@ point init_point(int x, int y)
 
 
 /*
-** make_point
-** 返回数值为下一个坐标位置的point类型结构体。
+** printf_matrix
 */
-point make_point( point spore, int n )
-{
-	int temp_x = spore.x;
-	int temp_y = spore.y;
-
-	if(roll(2))
-		spore.x += n;
-	else
-		spore.y += n;
-	if((spore.x < 0 || spore.x >= MAX) || (spore.y < 0 || spore.y >= MAX)) {
-	      spore.x = temp_x;
-	      spore.y = temp_y;
-	}
-
-	return spore;
-}
-
-
-/*
-** one_step
-** 返回 -1到1之间随机整数。
-*/
-int one_step( void )
-{
-	return (roll(3) - 1);
-}
-
-
-/*
-** printf_space
-** 打印整个space数组，显示到屏幕上。
-*/
-void printf_space( char (*s)[MAX] )
+void printf_matrix( char (*s)[MAX] )
 {
 	int i, j;
 	for(i = 0; i < MAX*2 + 2; i++)
@@ -173,7 +180,6 @@ void printf_space( char (*s)[MAX] )
 
 /*
 ** is_win
-** 矩阵中的数字按顺序排列时，返回1。
 */
 int is_win( char(*s)[MAX] )
 {
@@ -185,4 +191,25 @@ int is_win( char(*s)[MAX] )
 				return 0;
 	return 1;
 }
+
+
+
+/*
+** find_point
+*/
+point find_point( char(*s)[MAX] )
+{
+	point temp = {0, 0};
+	int i, j;
+	for(i = 0; i < MAX; i++, s++) 
+		for(j = 0; j < MAX; j++)
+			if((*s)[j] == ' ') {
+				temp.x = i;
+				temp.y = j;
+				return temp;
+			}
+	printf("No space!Error!\n");
+	exit(0);
+}
+
 

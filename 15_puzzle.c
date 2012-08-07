ef3605c2383d48cffaf00d15aa8956dc30767278
cforth <cforth@163.com>
2012-08-07 16:20:31 +0800

    改写了矩阵初始化模块，使之不会出现无解的情况

--- 15_puzzle.c
+++ 15_puzzle.c
@@ -2,29 +2,32 @@
 ** 重排十五,数字推盘游戏
 ** 将矩阵中的数字按照从小到大排列。
 ** 数字使用16进制表示：1-9,A-F
+** 不存在无解的情况。
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 #include <termios.h>
 #include <unistd.h>
 
-/* MAX为矩阵边长。
+/* 
+** MAX为矩阵边长。
 ** NUM为矩阵中的数字标号个数，必须是最大个数减1。
 ** CLASS为起始数字标号。
 */
 #define MAX 4
 #define NUM 15
 #define CLASS '1'
+#define MAX_STEP 100000
 
-/* 定义point类型结构体。
+/*
+** 定义Point类型结构体。
 ** 用来保存矩阵中单元的横坐标和纵坐标。
 */
 typedef struct {
 	int x;
 	int y;
-} point;
+} Point;
 
 /*
 ** roll
@@ -33,12 +36,6 @@ typedef struct {
 */
 int roll( int max_num );
 
-/*
-** init_point
-** 初始化point，返回point类型结构体。
-** 用来给结构体赋值, 初始化矩阵的起始状态。
-*/
-point init_point(int x, int y);
 
 /*
 ** printf_matrix
@@ -63,76 +60,94 @@ int my_getch( void );
 
 /*
 ** find_point
-** 寻找matrix矩阵中的空格位置，将坐标存入point类型的结构体返回。
+** 寻找matrix矩阵中的空格位置，将坐标存入Point类型的结构体返回。
 ** 用来找出指定矩阵中的空位，确定能够移动的数字标号的位置。
 */
-point find_point( char(*s)[MAX] );
+Point find_point( char(*s)[MAX] );
 
 
 int main( void )
 {
-	int i, c;
+	int i, j, c;
 	unsigned long step;
 	char matrix[MAX][MAX];
 	char class;
 
-	/*初始化随机数发生器、matrix矩阵、spoce结构体数组*/
+	/*初始化随机数发生器、matrix矩阵、Site结构体*/
 	srand((unsigned)time(0));
-	point spore[NUM];
-	point site;
-	memset( spore, 0, sizeof(spore) );
-	memset( matrix, ' ', sizeof(matrix) );
+	Point Site;
 	
-	/*初始化数字标号随机乱序的矩阵matrix*/
+	/*初始化矩阵matrix，填入顺序的数字标号*/
 	printf("I'm initializing...\n");
 
-	for(i = 0, class = CLASS; i < NUM; i++) {
-		spore[i] = init_point(roll(MAX), roll(MAX));
-		
-		while(matrix[spore[i].x][spore[i].y] != ' ')
-			spore[i] = init_point(roll(MAX), roll(MAX));
-		
-		matrix[spore[i].x][spore[i].y]= class;
-		
-		if(class == '9')
-			class = 'A';
-		else
-			class++;
-	}
+	for(i = 0, class = CLASS; i < MAX; i++)
+		for(j = 0; j < MAX; j++) {
+			matrix[i][j]= class;
+			if(class == '9')
+				class = 'A';
+			else
+				class++;
+		}
+	matrix[MAX - 1][MAX - 1] = ' ';
 
+	/*随机移动矩阵matrix中的数字标号，作为游戏起始状态*/
+	for(step = 0; step < MAX_STEP; step++) {
+		c = roll(4);
+		Site = find_point( &matrix[0] );
+		switch(c) {
+		case 0:
+			if((Site.y + 1) < MAX) {
+				matrix[Site.x][Site.y] = matrix[Site.x][Site.y + 1];
+				matrix[Site.x][Site.y + 1] = ' ';
+			} break;
+		case 1:
+			if((Site.y - 1) >= 0) {
+				matrix[Site.x][Site.y] = matrix[Site.x][Site.y - 1];
+				matrix[Site.x][Site.y - 1] = ' ';
+			} break;
+		case 2:
+			if((Site.x - 1) >= 0) {
+				matrix[Site.x][Site.y] = matrix[Site.x - 1][Site.y];
+				matrix[Site.x - 1][Site.y] = ' ';
+			} break;
+		case 3:
+			if((Site.x + 1) < MAX) {
+				matrix[Site.x][Site.y] = matrix[Site.x + 1][Site.y];
+				matrix[Site.x + 1][Site.y] = ' ';
+			} break;
+		}		
+	}
 	printf_matrix( &matrix[0] );
 	printf("I'm working...\n");
 
-	/*从键盘接收命令，执行相应的动作，并打印出矩阵*/
+	/*游戏部分，从键盘接收命令，执行相应的动作，并打印出矩阵*/
 	for(step = 0; is_win(&matrix[0]) != 1; step++) {
 		c = my_getch();
-		site = find_point( &matrix[0] );
+		Site = find_point( &matrix[0] );
 		switch(c) {
 		case 'a':
-			if((site.y + 1) < MAX) {
-				matrix[site.x][site.y] = matrix[site.x][site.y + 1];
-				matrix[site.x][site.y + 1] = ' ';
+			if((Site.y + 1) < MAX) {
+				matrix[Site.x][Site.y] = matrix[Site.x][Site.y + 1];
+				matrix[Site.x][Site.y + 1] = ' ';
 			} break;
 		case 'd':
-			if((site.y - 1) >= 0) {
-				matrix[site.x][site.y] = matrix[site.x][site.y - 1];
-				matrix[site.x][site.y - 1] = ' ';
+			if((Site.y - 1) >= 0) {
+				matrix[Site.x][Site.y] = matrix[Site.x][Site.y - 1];
+				matrix[Site.x][Site.y - 1] = ' ';
 			} break;
 		case 's':
-			if((site.x - 1) >= 0) {
-				matrix[site.x][site.y] = matrix[site.x - 1][site.y];
-				matrix[site.x - 1][site.y] = ' ';
+			if((Site.x - 1) >= 0) {
+				matrix[Site.x][Site.y] = matrix[Site.x - 1][Site.y];
+				matrix[Site.x - 1][Site.y] = ' ';
 			} break;
 		case 'w':
-			if((site.x + 1) < MAX) {
-				matrix[site.x][site.y] = matrix[site.x + 1][site.y];
-				matrix[site.x + 1][site.y] = ' ';
+			if((Site.x + 1) < MAX) {
+				matrix[Site.x][Site.y] = matrix[Site.x + 1][Site.y];
+				matrix[Site.x + 1][Site.y] = ' ';
 			} break;
 		case '>':
 			printf("Used %lu steps.\n", step);
 			exit(0);
-		default:
-			break;
 		}		
 		printf_matrix( &matrix[0] );
 	}
@@ -154,14 +169,14 @@ int roll( int max_num )
 
 
 /*
-** init_point
+** init_Point
 */
-point init_point( int x, int y )
+Point init_Point( int x, int y )
 {
-	point temp;
-	temp.x = x;
-	temp.y = y;
-	return temp;
+	Point Temp;
+	Temp.x = x;
+	Temp.y = y;
+	return Temp;
 }
 
 
@@ -181,7 +196,7 @@ void printf_matrix( char (*s)[MAX] )
 		      printf("%c ", (*s)[j]);
 		printf("|\n");
 	}
-
+	
 	for(i = 0; i < MAX*2 + 2; i++)
 		printf("-");
 	printf("\n");
@@ -230,16 +245,16 @@ int my_getch(void)
 /*
 ** find_point
 */
-point find_point( char(*s)[MAX] )
+Point find_point( char(*s)[MAX] )
 {
-	point temp = {0, 0};
+	Point Temp = {0, 0};
 	int i, j;
 	for(i = 0; i < MAX; i++, s++) 
 		for(j = 0; j < MAX; j++)
 			if((*s)[j] == ' ') {
-				temp.x = i;
-				temp.y = j;
-				return temp;
+				Temp.x = i;
+				Temp.y = j;
+				return Temp;
 			}
 	printf("No space!Error!\n");
 	exit(0);

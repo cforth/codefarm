7a60fe2defb91d2f1fb9abd6db780b9399f4d1b0
xiaohao@cforth <cforth@163.com>
2012-08-10 21:15:30 +0800

    实现了类似cat的功能

--- todolist/ToDoList.c
+++ todolist/ToDoList.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
+#include <time.h>
 
-void filecopy(F)
+void filecopy(FILE *, FILE*);
+
 int main( int argc, char *argv[] )
 {
-	int c;
 	FILE *fp;
-
-	while((c = getchar()) != EOF)
-		putchar(c);
+	
+	if(argc == 1)
+		filecopy(stdin, stdout);
+	else
+		while(--argc > 0)
+			if((fp = fopen(*++argv, "r")) == NULL) {
+				printf("ToDoList: can't open %s\n", *argv);
+				return 1;
+			}
+			else {
+				filecopy(fp, stdout);
+				fclose(fp);
+			}
 	return 0;
+}
+
+
+void filecopy(FILE *ifp, FILE *ofp)
+{
+	int c;
+
+	while((c = getc(ifp)) != EOF) {
+		switch(c){
+		case '&':
+			printf("2012-08-10");
+			break;
+		default :
+			putc(c, ofp);
+		}
+	}
 }

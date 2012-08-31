022225ed6ad6f6acc40e1219338b542d2153b5c0
cforth <cforth@163.com>
2012-08-31 16:16:05 +0800

    用宏创建泛型的一次尝试

--- hanoi_game/hanoi_tower.c
+++ hanoi_game/hanoi_tower.c
@@ -4,29 +4,118 @@
 #include<unistd.h>
 #define TRUE 1
 #define FALSE 0
-#define MAX 5
-#define MAX_FACT 15		//MAX!
+#define MAX 3
+#define MAX_FACT 6
 #define ON_A 1
 #define ON_B 2
 #define ON_C 3
 
-int my_getch( void );
-int a_to_b( void );
-int a_to_c( void );
-int b_to_a( void );
-int b_to_c( void );
-int c_to_a( void );
-int c_to_b( void );
-int show_hanoi( void );
-int dohanoi(int n,int a,int b,int c); 
-int init_hanoi(int max_num);
-int is_complete( int* top );
 
 int a_tower[MAX+1], b_tower[MAX+1], c_tower[MAX+1];
 int *a_top = &a_tower[MAX], *b_top = b_tower, *c_top = c_tower;
 int now_status = ON_A;
 int catch_status = FALSE;
-	
+
+
+#define X_TO_Y(X, Y)						\
+int X##_to_##Y ()							\
+{											\
+	if(( X##_top == &X##_tower[0] ) 		\
+		|| ( Y##_top == &Y##_tower[MAX] ) 	\
+		|| ( *Y##_top < *X##_top )) {		\
+		catch_status = FALSE;				\
+		return 0;							\
+	}										\
+	Y##_top++;								\
+	*Y##_top = *X##_top;					\
+	*X##_top = 0;							\
+	X##_top--;								\
+	return 0;								\
+}
+
+
+X_TO_Y(a, b)
+
+
+X_TO_Y(b, a)
+
+
+X_TO_Y(a, c)
+
+
+X_TO_Y(c, a)
+
+
+X_TO_Y(b, c)
+
+
+X_TO_Y(c, b)
+
+
+int show_hanoi()
+{
+	int i;
+	for(i=1; i<now_status; i++)
+		printf("\t");
+	if(catch_status == FALSE)
+		printf("\t\t\t\t*\n");
+	else
+		printf("\t\t\t\t!\n");
+	for(i=MAX; i>0; i--)
+		printf("\t\t\t\t%d\t%d\t%d\n",a_tower[i], b_tower[i], c_tower[i]);
+	printf("\t\t\t\tA\tB\tC");
+	printf("\n\n\n\n\n\n\n\n\n\n\n");
+	return 0;
+}
+
+
+int init_hanoi(int max_num)
+{
+	int i;
+	for(i=1; i<=MAX; i++,max_num--)
+		a_tower[i] = max_num;
+	a_tower[0] = b_tower[0] = c_tower[0] = 100;
+	return 0;
+}
+
+
+int  dohanoi(int n,int a,int b,int c) 
+{
+	if(n==1)
+		printf("%c->%c ",a,c);      
+	else    {
+		dohanoi(n-1,a,c,b);                     
+		printf("%c->%c ",a,c);         
+		dohanoi(n-1,b,a,c);   
+	} 
+	return 0;
+} 
+
+
+int my_getch(void)
+{
+	struct termios oldt,
+	newt;
+	int ch;
+	tcgetattr( STDIN_FILENO, &oldt );
+	newt = oldt;
+	newt.c_lflag &= ~( ICANON | ECHO );
+	tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+	ch = getchar();
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+	return ch;
+}
+
+
+int is_complete( int* top )
+{
+	int sum = 0;
+	for(; top > c_tower; top-- )
+		sum += *top;
+	return (sum == MAX_FACT);
+}
+
+
 int main()
 {
 	char c;
@@ -112,143 +201,4 @@ int main()
 	}
 	printf("Good Game! Bye!\n");
 	return 0;	
-}
-
-int a_to_b()
-{
-	if((a_top == &a_tower[0]) || (b_top == &b_tower[MAX]) || (*b_top < *a_top)) {
-		catch_status = FALSE;
-		return 0;
-	}
-	b_top++;
-	*b_top = *a_top;
-	*a_top = 0;
-	a_top--;
-	return 0;
-}
-
-int a_to_c()
-{
-	if((a_top == &a_tower[0]) || (c_top == &c_tower[MAX]) || (*c_top < *a_top)) {
-		catch_status = FALSE;
-		return 0;
-	}
-	c_top++;
-	*c_top = *a_top;
-	*a_top = 0;
-	a_top--;
-	return 0;
-}
-
-int b_to_a()
-{
-	if((b_top == &b_tower[0]) || (a_top == &a_tower[MAX]) || (*a_top < *b_top)) {
-		catch_status = FALSE;
-		return 0;
-	}
-	a_top++;
-	*a_top = *b_top;
-	*b_top = 0;
-	b_top--;
-	return 0;
-}
-int b_to_c()
-{
-	if((b_top == &b_tower[0]) || (c_top == &c_tower[MAX]) || (*c_top < *b_top)) {
-		catch_status = FALSE;
-		return 0;
-	}
-	c_top++;
-	*c_top = *b_top;
-	*b_top = 0;
-	b_top--;
-	return 0;
-}
-
-int c_to_a()
-{
-	if((c_top == &c_tower[0]) || (a_top == &a_tower[MAX]) || (*a_top < *c_top)) {
-		catch_status = FALSE;
-		return 0;
-	}
-	a_top++;
-	*a_top = *c_top;
-	*c_top = 0;
-	c_top--;
-	return 0;
-}
-
-int c_to_b()
-{
-	if((c_top == &c_tower[0]) || (b_top == &b_tower[MAX]) || (*b_top < *c_top)) {
-		catch_status = FALSE;
-		return 0;
-	}
-	b_top++;
-	*b_top = *c_top;
-	*c_top = 0;
-	c_top--;
-	return 0;
-}
-
-int show_hanoi()
-{
-	int i;
-	for(i=1; i<now_status; i++)
-		printf("\t");
-	if(catch_status == FALSE)
-		printf("\t\t\t\t*\n");
-	else
-		printf("\t\t\t\t!\n");
-	for(i=MAX; i>0; i--)
-		printf("\t\t\t\t%d\t%d\t%d\n",a_tower[i], b_tower[i], c_tower[i]);
-	printf("\t\t\t\tA\tB\tC");
-	printf("\n\n\n\n\n\n\n\n\n\n\n");
-	return 0;
-}
-
-int init_hanoi(int max_num)
-{
-	int i;
-	for(i=1; i<=MAX; i++,max_num--)
-		a_tower[i] = max_num;
-	a_tower[0] = b_tower[0] = c_tower[0] = 100;
-	return 0;
-}
-
-int  dohanoi(int n,int a,int b,int c) 
-{
-	if(n==1)
-		printf("%c->%c ",a,c);      
-	else    {
-		dohanoi(n-1,a,c,b);                     
-		printf("%c->%c ",a,c);         
-		dohanoi(n-1,b,a,c);   
-	} 
-	return 0;
-} 
-
-
-
-int my_getch(void)
-{
-	struct termios oldt,
-	newt;
-	int ch;
-	tcgetattr( STDIN_FILENO, &oldt );
-	newt = oldt;
-	newt.c_lflag &= ~( ICANON | ECHO );
-	tcsetattr( STDIN_FILENO, TCSANOW, &newt );
-	ch = getchar();
-	tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
-	return ch;
-}
-
-
-int is_complete( int* top )
-{
-	int sum = 0;
-	for(; top > c_tower; top-- )
-		sum += *top;
-	return (sum == MAX_FACT);
 }

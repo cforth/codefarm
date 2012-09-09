a3de1c4cea1448d2de45c5528fd60501ca13a10c
xiaohao@cforth <cforth@163.com>
2012-09-09 09:30:14 +0800

    练习

--- lua/lib1.lua
+++ lua/lib1.lua
@@ -4,3 +4,16 @@ function norm (x, y)
 	local n2 = x^2 + y^2
 	return math.sqrt(n2)
 end
+
+
+function maximum (a)
+	local mi = 1
+	local m = a[mi]
+	for i,val in ipairs(a) do
+		if val > m then
+			mi = i
+			m = val
+		end
+	end
+	return m,mi
+end

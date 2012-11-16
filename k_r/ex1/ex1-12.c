c6e43e7442d041c01c9326593cfa133e647d6087
cforth <cforth@163.com>
2012-11-16 15:50:31 +0800

    把it's这种模式的单词作为一个单词

--- k_r/ex1/ex1-12.c
+++ k_r/ex1/ex1-12.c
@@ -17,7 +17,8 @@ int main()
 	while ((c = getchar()) != EOF) {
 		if ((c >= '0' && c <= '9')
 			|| (c >= 'a' && c <= 'z')
-			|| (c >= 'A' && c <= 'Z'))
+			|| (c >= 'A' && c <= 'Z')
+			|| c == '\'')
 			state = IN;
 		else 
 			state = OUT;

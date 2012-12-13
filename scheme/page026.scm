f2d3c7777d427c3728edeac571b41ba87a7304ed
cforth <cforth@163.com>
2012-12-13 10:38:42 +0800

    换零钱方案

---
+++ scheme/page026.scm
@@ -0,0 +1,29 @@
+#!/bin/guile -s
+!#
+
+;换零钱方案，最小单位为1元人民币
+(define (count-change amount)
+	(cc amount 6))
+
+(define (cc amount kinds-of-coins)
+	(cond 
+		((= amount 0) 1)
+		((or (< amount 0) (= kinds-of-coins 0)) 0)
+		(else
+			(+	(cc amount
+					(- kinds-of-coins 1))
+				(cc (- amount
+						(first-denomination kinds-of-coins))
+					kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+	(cond
+		((= kinds-of-coins 1) 1)
+		((= kinds-of-coins 2) 5)
+		((= kinds-of-coins 3) 10)
+		((= kinds-of-coins 4) 20)
+		((= kinds-of-coins 5) 50)
+		((= kinds-of-coins 6) 100)))
+
+(display (count-change 100))
+(newline)

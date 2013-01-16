e8edafe1a3dd1e38a1340a6c93ab34910987b7cc
cforth <cforth@163.com>
2013-01-16 14:37:33 +0800

    打印出区间范围内的所有素数

--- scheme/ex1-21.scm
+++ scheme/ex1-21.scm
@@ -19,9 +19,27 @@
 (define (prime? n)
 	(= n (smallest-divisor n)))
 
-(display (smallest-divisor 199))
-(newline)
-(display (smallest-divisor 1999))
-(newline)
-(display (smallest-divisor 19999))
+;测试用例
+;(display (smallest-divisor 199))
+;(newline)
+;(display (smallest-divisor 1999))
+;(newline)
+;(display (smallest-divisor 19999))
+;(newline)
+
+
+;打印出区间范围内所有的素数
+(define (print-prime start end)
+	(if (even? start) (print-prime (+ start 1) end)
+		(cond ((> end start) 
+				(if (prime? start)
+					(display-p start))
+				(print-prime (+ start 2) end)))))
+
+(define (display-p n)
+	(display n)
+	(display " "))
+
+;测试用例
+(print-prime 9999900 10000000)
 (newline)

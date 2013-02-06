67292e3f944131804b13378858126dc32676de2b
cforth <cforth@163.com>
2013-02-06 09:39:31 +0800

    对树的映射，继续抽象

--- scheme/ex2/ex2-31.scm
+++ scheme/ex2/ex2-31.scm
@@ -7,7 +7,7 @@
 
 (define (tree-map proc tree)
     (map (lambda (sub-tree)
-	    (if (pair? sub-tree)
+        (if (pair? sub-tree)
             (tree-map proc sub-tree)
             (proc sub-tree)))
         tree))

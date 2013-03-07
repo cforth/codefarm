532412a745ff4c310409f16fc48ba00130571e4e
cforth <cforth@163.com>
2013-03-07 12:44:08 +0800

    修改了汉诺塔初始化函数，修改了汉诺塔实现模块wq

--- hanoi_game/hanoi_2013.scm
+++ hanoi_game/hanoi_2013.scm
@@ -19,7 +19,7 @@
 
 (define (front-stack stack)
     (if (empty-stack? stack)
-        (error "FRONT called with an empty stack" stack)
+        '()                      ;当堆栈为空时，取顶端返回空表
         (car (front-ptr stack))))
 
 
@@ -49,29 +49,36 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;汉诺塔模块内部实现
+;使用堆栈模块函数做为接口，操作汉诺塔
 
 ;初始化一个塔
-(define (init-hanoi level hanoi)
-    (if (= level 0)
-        hanoi
-        (init-hanoi (- level 1) (push-stack! hanoi level))))
+(define (init-hanoi level)
+    (define (init-hanoi-iter level hanoi)
+        (if (= level 0)
+            hanoi
+            (init-hanoi-iter (- level 1) (push-stack! hanoi level))))
+    (init-hanoi-iter level (make-stack)))
 
 ;汉诺塔完成度检测，汉诺塔游戏是否完成？
 (define (complete-hanoi? level hanoi)
-    (if (= level (length (front-ptr hanoi)))
+    (if (= level (length (print-stack hanoi)))
         #t
         #f))
 
+;打印出一个汉诺塔
+(define (print-hanoi hanoi)
+    (print-stack hanoi))
+
 ;汉诺塔移塔规则
 (define (can-be-push? hanoi item)
-    (if (null? (front-ptr hanoi))
+    (if (null? (front-stack hanoi))
         #t
         (if (> (front-stack hanoi) item)
             #t
             #f)))
 
 (define (can-be-pop? hanoi)
-    (if (null? (front-ptr hanoi))
+    (if (null? (front-stack hanoi))
         #f
         #t))
 
@@ -99,80 +106,87 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;汉诺塔非递归算法模块
+;待完成
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;测试，汉诺塔雏形
-(define h1 (make-stack))
+(define h1 (init-hanoi 3))
 
-(define h2 (make-stack))
+(define h2 (init-hanoi 0))
 
-(define h3 (make-stack))
+(define h3 (init-hanoi 0))
 
-(init-hanoi 3 h1)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 
 (move-hanoi h2 h1)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 
 (move-hanoi h1 h3)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 
 (move-hanoi h1 h2)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 
 (move-hanoi h3 h2)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 
 (move-hanoi h1 h3)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 
 (move-hanoi h2 h1)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 
 (move-hanoi h2 h3)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 
 (move-hanoi h1 h3)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 
 (move-hanoi h1 h3)
-(display (print-stack h1))
-(display (print-stack h2))
-(display (print-stack h3))
+(display (print-hanoi h1))
+(display (print-hanoi h2))
+(display (print-hanoi h3))
 (newline)
 (newline)
 

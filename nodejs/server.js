a3abd0717b9fd98698e5ea2fb94f85d009f3fa5d
cforth <cforth@163.com>
2013-12-16 16:55:28 +0800

    nodejs学习

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,13 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
   function onRequest(request, response) {
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();

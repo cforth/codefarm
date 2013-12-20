dc913fab96a53d0975ee4c88e4d48d6854726a88
cforth <cforth@163.com>
2013-12-20 14:44:09 +0800

    node练习

---
+++ nodejs/requestHandlers.js
@@ -0,0 +1,19 @@
+function start() {
+  console.log("Request handler 'start' was called.");
+
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+
+  sleep(10000);
+  return "Hello Start";
+}
+
+function upload() {
+  console.log("Request handler 'upload' was called.");
+  return "Hello Upload";
+}
+
+exports.start = start;
+exports.upload = upload;
--- nodejs/router.js
+++ nodejs/router.js
@@ -1,5 +1,11 @@
-function route(pathname) {
+function route(handle, pathname) {
   console.log("About to route a request for " + pathname);
+  if (typeof handle[pathname] === 'function') {
+    return handle[pathname]();
+  } else {
+    console.log("No request handler found for " + pathname);
+    return "404 Not found";
+  }
 }
 
 exports.route = route;

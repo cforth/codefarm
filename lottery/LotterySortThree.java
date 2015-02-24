6cf6fe6e1d1ac9bed0975d573efb2279dac1336d
xiaohao@cforth <cforth@163.com>
2015-02-24 19:23:22 +0800

    LotterySortThree:静态方法

--- lottery/LotterySortThree.java
+++ lottery/LotterySortThree.java
@@ -2,9 +2,42 @@ import java.util.*;
 
 public class LotterySortThree {
     public static void main(String[] args) {
+
+        int[] nums = LotterySortThree.EnterThreeNums();
+        int[] randomNums = LotterySortThree.MakeRandomNums(3);
+
+        System.out.println("Your Numbers:");
+        for(int i: nums)
+            System.out.print(i + " ");
+        System.out.println();
+        
+        System.out.println("Lottery Numbers:");
+        for(int i: randomNums)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+
+
+    /*
+     * MakeRandomNums 静态方法
+     * 读取参数n，返回n个随机数字的数组
+     */
+    public static int[] MakeRandomNums(int n) {
+        int[] randomNums = new int[n];
+        for(int i=0; i<n; i++)
+            randomNums[i] = (int)(Math.random() * 10);
+
+        return randomNums;
+    }
+
+
+    /*
+     * EnterThreeNums 静态方法
+     * 从控制台读取输入的三个数字，返回这个数组
+     */
+    public static int[] EnterThreeNums() {
         Scanner in = new Scanner(System.in);
         
-        //读取控制台输入的三个数字，用空格分开
         System.out.print("Enter Three numbers: ");
         String numStr = in.nextLine();
         numStr = numStr.trim().replaceAll("\\s{1,}", " ");
@@ -20,19 +53,6 @@ public class LotterySortThree {
         for(int i=0; i<3; i++)
             nums[i] = Integer.parseInt(numArray[i]);
         
-        //随机生成三个数字
-        int[] randomNums = new int[3];
-        for(int i=0; i<3; i++)
-            randomNums[i] = (int)(Math.random() * 10);
-        
-        System.out.println("Your Numbers:");
-        for(int i: nums)
-            System.out.print(i + " ");
-        System.out.println();
-        
-        System.out.println("Lottery Numbers:");
-        for(int i: randomNums)
-            System.out.print(i + " ");
-        System.out.println();
+        return nums;
     }
 }

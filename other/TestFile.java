d7f840c438ab052c888f4c7295bbfb6bf4bb22a7
Chai Fei <cforth@163.com>
2016-04-02 19:14:42 +0800

    简易资源管理器，并且输出到文件

--- other/TestFile.java
+++ other/TestFile.java
@@ -7,29 +7,44 @@ public class TestFile {
 
 	public static void main(String[] args) throws Exception{
 		//列出所有子目录中的文件
-		print(new File("F:" + File.separator), 0);
+		print(new File("F:" + File.separator), new File("F:" + File.separator + "demo" + File.separator + "test.txt"), 0);
 	}
 	
-	public static void print(File file, int level) {
+	public static void print(File readfile, File writefile, int level) throws Exception {
 		StringBuffer str = new StringBuffer();
 		for (int x = 0; x < level; x++) {
 			str.append("|\t");
 		}
 		str.append("|-");
-		System.out.println(str + file.getName() + "\t\t" 
-				+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(file.lastModified())) + "\t\t"
-				+ (file.isDirectory() ? "文件夹" : "文件") + "\t\t"
-				+ (file.isDirectory() ? " " : (new BigDecimal((double)file.length()/1024/1024).divide(new BigDecimal(1), 2, BigDecimal.ROUND_HALF_UP)) + "M"));
+		writeFile(writefile,
+				str + readfile.getName() + "\t\t" 
+				+ new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(readfile.lastModified())) + "\t\t"
+				+ (readfile.isDirectory() ? "文件夹" : "文件") + "\t\t"
+				+ (readfile.isDirectory() ? "\r\n" : (new BigDecimal((double)readfile.length()/1024/1024).divide(new BigDecimal(1), 2, BigDecimal.ROUND_HALF_UP)) + "M\r\n"));
 		
-		if(file.isDirectory()) {
+		if(readfile.isDirectory()) {
 			level++;
-			File result[] = file.listFiles();
+			File result[] = readfile.listFiles();
 			if(result != null) {
 				for(int x = 0; x < result.length; x ++) {
-					print(result[x], level);
+					print(result[x], writefile, level);
 				}
 			}
 		}
+	}
+	
+	public static void writeFile(File file, String str) throws Exception {
+		// 1、目录不存在需要先创建目录
+		if(!file.getParentFile().exists()) { //首先判断父目录是否存在
+			file.getParentFile().mkdirs(); //不存在的话创建目录
+		}
+		// 2、使用OutputStream和其子类进行对象的实例化
+		OutputStream output = new FileOutputStream(file, true);
+		// 3、要进行文件内容的输出
+		byte data[] = str.getBytes();
+		output.write(data);;
+		// 4、资源操作一定要进行关闭
+		output.close();
 	}
 
 }

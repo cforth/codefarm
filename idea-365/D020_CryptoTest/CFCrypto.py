43a078f435a14ff05555f88d64ac74e56dab7c65
chaif <cforth@163.com>
2017-06-11 08:22:17 +0800

    支持RSA加密文件

--- idea-365/D020_CryptoTest/CFCrypto.py
+++ idea-365/D020_CryptoTest/CFCrypto.py
@@ -3,6 +3,9 @@ import os
 import struct
 import base64
 from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import PKCS1_OAEP
 
 
 # PKCS5填充方式，将数据填充为multiple_of_byte的整数倍
@@ -198,3 +201,83 @@ class DirNameCrypto(object):
         dir_path = os.path.abspath(dir_path)
         DirNameCrypto.handle(dir_path, self.string_crypto.decrypt)
         os.chdir(root_path)
+
+
+# RSA加密解密类
+class RSACrypto(object):
+    def __init__(self):
+        self.code = None
+        self.private_key_path = None
+        self.public_key_path = None
+
+    # 设置私钥密码
+    def set_password(self, password):
+        self.code = password
+
+    # 设置私钥文件的路径
+    def set_private_key_path(self, private_key_path):
+        self.private_key_path = private_key_path
+
+    # 设置公钥文件的路径
+    def set_public_key_path(self, public_key_path):
+        self.public_key_path = public_key_path
+
+    # 根据私钥密码生成私钥和公钥
+    def generate_key(self):
+        if not self.code:
+            print('Please set password first!')
+        elif not self.private_key_path:
+            print('Please set private_key_path!')
+        elif not self.public_key_path:
+            print('Please set public_key_path!')
+        else:
+            key = RSA.generate(2048)
+            encrypted_key = key.exportKey(passphrase=self.code, pkcs=8,
+                                          protection="scryptAndAES128-CBC")
+            with open(self.private_key_path, 'wb') as f:
+                f.write(encrypted_key)
+            with open(self.public_key_path, 'wb') as f:
+                f.write(key.publickey().exportKey())
+
+    # 加密文件，需要设置公钥文件
+    def encrypt(self, file_path, output_file_path):
+        if not self.public_key_path:
+            print('Please set public_key_path!')
+        else:
+            with open(output_file_path, 'wb') as out_file:
+                public_file = open(self.public_key_path)
+                recipient_key = RSA.import_key(
+                    public_file.read())
+                session_key = get_random_bytes(16)
+                cipher_rsa = PKCS1_OAEP.new(recipient_key)
+                out_file.write(cipher_rsa.encrypt(session_key))
+                cipher_aes = AES.new(session_key, AES.MODE_EAX)
+                public_file.close()
+                with open(file_path, 'rb') as f:
+                    data = f.read()
+                cipher_text, tag = cipher_aes.encrypt_and_digest(data)
+                out_file.write(cipher_aes.nonce)
+                out_file.write(tag)
+                out_file.write(cipher_text)
+
+    # 解密文件，需要设置私钥密码和私钥文件
+    def decrypt(self, file_path, output_file_path):
+        if not self.code:
+            print('Please set password first!')
+        elif not self.private_key_path:
+            print('Please set private_key_path!')
+        else:
+            with open(file_path, 'rb') as file:
+                private_file = open(self.private_key_path)
+                private_key = RSA.import_key(
+                    private_file.read(),
+                    passphrase=self.code)
+                enc_session_key, nonce, tag, cipher_text = [file.read(x)
+                                                            for x in (private_key.size_in_bytes(), 16, 16, -1)]
+                cipher_rsa = PKCS1_OAEP.new(private_key)
+                session_key = cipher_rsa.decrypt(enc_session_key)
+                cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+                data = cipher_aes.decrypt_and_verify(cipher_text, tag)
+                private_file.close()
+            with open(output_file_path, 'wb') as f:
+                f.write(data)
--- idea-365/D020_CryptoTest/test_crypto.py
+++ idea-365/D020_CryptoTest/test_crypto.py
@@ -1,4 +1,3 @@
-from CFCrypto import *
 import os
 import filecmp
 import unittest
@@ -39,6 +38,27 @@ class TestCrypto(unittest.TestCase):
         self.assertTrue(os.path.exists('./en_data/testdata/测试中文目录名/empytdir'))
         shutil.rmtree('./en_data')
         shutil.rmtree('./de_data')
+
+    def test_RSACrypto(self):
+        # 使用RSA加密解密的演示
+        my_rsa = RSACrypto()
+        # 如果首次加密，本地没有私钥文件和公钥文件，则需要生成
+        my_rsa.set_password('helloRSA')
+        my_rsa.set_public_key_path('./testdata/my_rsa.pub')
+        my_rsa.set_private_key_path('./testdata/my_rsa')
+        my_rsa.generate_key()
+        # 使用RSA加密需要用到公钥文件
+        my_rsa.set_public_key_path('./testdata/my_rsa.pub')
+        my_rsa.encrypt('./testdata/test.jpg', './testdata/test.bin')
+        # 使用RSA解密需要用到私钥密码和私钥文件
+        my_rsa.set_password('helloRSA')
+        my_rsa.set_private_key_path('./testdata/my_rsa')
+        my_rsa.decrypt('./testdata/test.bin', './testdata/output.jpg')
+        self.assertTrue(filecmp.cmp('./testdata/test.jpg', './testdata/output.jpg'))
+        os.remove('./testdata/output.jpg')
+        os.remove('./testdata/test.bin')
+        os.remove('./testdata/my_rsa.pub')
+        os.remove('./testdata/my_rsa')
 
 
 if __name__ == '__main__':

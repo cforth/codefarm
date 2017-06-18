9a6f8064a0aee1442e3229296917c6269d05170d
chaif <cforth@163.com>
2017-06-18 14:52:31 +0800

    优化过滤器代码

--- idea-365/D021_CFSearch/CFSearch.py
+++ idea-365/D021_CFSearch/CFSearch.py
@@ -1,22 +1,22 @@
 import os
 
 
-class CoolFileSearch(object):
-    movie_filter = ['.mp4', '.MP4', '.avi', '.AVI', '.wmv', '.WMV',
-                    '.rmvb', '.RMVB', '.mov', '.MOV', '.mkv', '.MKV']
-    picture_filter = ['.bmp', '.BMP', '.gif', '.GIF', '.jpeg', '.JGEG',
-                      '.jpg', '.JPG', '.png', '.PNG']
-    data_filter = ['.zip', '.ZIP', '.rar', '.RAR', '.7z', '.7Z']
-    doc_filter = ['.doc', '.xls', '.ppt', '.pdf']
+class FileSearch(object):
+    MOVIE = ['mp4', 'avi', 'wmv', 'rmvb', 'mov', 'mkv']
+    PIC = ['bmp', 'gif', 'jpeg', 'jpg', 'png']
+    DATA = ['zip', 'rar', '7z']
+    DOC = ['doc', 'xls', 'ppt', 'pdf', 'txt']
 
     # 初始化时设置好过滤器
     def __init__(self, the_filter):
-        self.the_filter = the_filter
+        capital_filter = [x.upper() for x in the_filter]
+        self.the_filter = the_filter + capital_filter
+        print(self.the_filter)
 
     # 搜索过滤列表内的后缀名文件，返回一个生成器
     def search(self, dir_path):
         for path, subdir, files in os.walk(dir_path):
             for f in files:
-                if '.' in f and f[f.rindex('.'):] in self.the_filter:
+                if '.' in f and f[f.rindex('.')+1:] in self.the_filter:
                     file_path = os.path.abspath(path)
-                    yield file_path + '\\' + f
+                    yield os.path.join(file_path, f)

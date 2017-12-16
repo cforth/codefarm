7ac52b450a5515799dd2876e233123f0273ee593
chaif <cforth@163.com>
2017-12-16 19:57:04 +0800

    添加变量绑定

--- idea-365/D028_XML2GUI/xml2gui.py
+++ idea-365/D028_XML2GUI/xml2gui.py
@@ -1,4 +1,5 @@
 from xml.parsers.expat import ParserCreate
+import tkinter as tk
 import tkinter.ttk as ttk
 import logging
 
@@ -53,24 +54,33 @@ class WindowMetaclass(type):
 class Window(ttk.Frame, metaclass=WindowMetaclass):
     def __init__(self, master=None):
         super().__init__(master, padding=2)
-        self.create_widgets_layout()
+        self.set_ui_from_xml()
         self.grid(row=0, column=0)
 
-    def create_widgets_layout(self):
-        widgets = Window.__dict__['my_widget'] if Window.__dict__.get('my_widget') else None
-        if widgets:
-            for k in widgets:
+    # 从xml文件自动设置UI控件
+    def set_ui_from_xml(self):
+        widget = Window.__dict__['my_widget'] if Window.__dict__.get('my_widget') else None
+        if widget:
+            for k in widget:
+                widget_str_parm = widget[k]["strParm"] if widget[k].get("strParm") else {}
+                widget_int_parm = widget[k]["intParm"] if widget[k].get("intParm") else {}
+                widget_var = widget[k]["var"] if widget[k].get("var") else None
+
                 # 动态生成控件，并添加字符串类型的参数（若有）
-                if widgets[k]["type"]["type"] == "Entry":
-                    self.__dict__[k] = ttk.Entry(self, **widgets[k]["strParm"])
-                elif widgets[k]["type"]["type"] == "Button":
-                    self.__dict__[k] = ttk.Button(self, **widgets[k]["strParm"])
-                elif widgets[k]["type"]["type"] == "Label":
-                    self.__dict__[k] = ttk.Label(self, **widgets[k]["strParm"])
+                if widget[k]["type"]["type"] == "Entry":
+                    self.__dict__[k] = ttk.Entry(self, **widget_str_parm)
+                elif widget[k]["type"]["type"] == "Button":
+                    self.__dict__[k] = ttk.Button(self, **widget_str_parm)
+                elif widget[k]["type"]["type"] == "Label":
+                    self.__dict__[k] = ttk.Label(self, **widget_str_parm)
                 # 为每个控件添加数值类型的参数（若有）
-                for pk in widgets[k]["intParm"]:
-                    self.__dict__[k][pk] = int(widgets[k]["intParm"][pk])
-                self.__dict__[k].grid(**widgets[k]["grid"])
+                for pk in widget_int_parm:
+                    self.__dict__[k][pk] = int(widget_int_parm[pk])
+                # 为每个控件绑定变量（若有）
+                if widget_var:
+                    self.__dict__[widget_var["name"]] = tk.StringVar()
+                    self.__dict__[k]["textvariable"] = self.__dict__[widget_var["name"]]
+                self.__dict__[k].grid(**widget[k]["grid"])
 
 
 app = Window()

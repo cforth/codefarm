949e4560e50b234f60873060fd5d55a905c2bce1
chaif <cforth@163.com>
2017-12-23 12:33:54 +0800

    :art:界面和逻辑完全分离

--- idea-365/D029_JSON2GUI/Calculator.py
+++ idea-365/D029_JSON2GUI/Calculator.py
@@ -3,78 +3,35 @@ import tkinter as tk
 
 
 class Window(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, ui_json, master=None):
         super().__init__(master)
-        # 从xml文件自动设置UI控件
-        create_ui(self, "UI.json")
-        create_bind(self, "oneButton", self.callback)
-        create_bind(self, "twoButton", self.callback)
-        create_bind(self, "threeButton", self.callback)
-        create_bind(self, "fourButton", self.callback)
-        create_bind(self, "fiveButton", self.callback)
-        create_bind(self, "sixButton", self.callback)
-        create_bind(self, "sevenButton", self.callback)
-        create_bind(self, "eightButton", self.callback)
-        create_bind(self, "nineButton", self.callback)
-        create_bind(self, "zeroButton", self.callback)
-        create_bind(self, "spotButton", self.callback)
-        create_bind(self, "plusButton", self.callback)
-        create_bind(self, "minusButton", self.callback)
-        create_bind(self, "mulButton", self.callback)
-        create_bind(self, "divButton", self.callback)
-        create_bind(self, "backButton", self.callback)
-        create_bind(self, "acButton", self.callback)
-        create_bind(self, "equalButton", self.callback)
-        create_bind(self, "modButton", self.callback)
+        # 从json自动设置UI控件
+        create_ui(self, ui_json)
+        # 从json自动绑定事件
+        create_all_binds(self, ui_json)
         self.cache = ""
         self.grid(row=0, column=0)
 
     def callback(self, event):
-        if event.widget == self.oneButton:
-            self.cache += "1"
-        elif event.widget == self.twoButton:
-            self.cache += "2"
-        elif event.widget == self.threeButton:
-            self.cache += "3"
-        elif event.widget == self.fourButton:
-            self.cache += "4"
-        elif event.widget == self.fiveButton:
-            self.cache += "5"
-        elif event.widget == self.sixButton:
-            self.cache += "6"
-        elif event.widget == self.sevenButton:
-            self.cache += "7"
-        elif event.widget == self.eightButton:
-            self.cache += "8"
-        elif event.widget == self.nineButton:
-            self.cache += "9"
-        elif event.widget == self.zeroButton:
-            self.cache += "0"
-        elif event.widget == self.spotButton:
-            self.cache += "."
-        elif event.widget == self.plusButton:
-            self.cache += "+"
-        elif event.widget == self.minusButton:
-            self.cache += "-"
-        elif event.widget == self.mulButton:
-            self.cache += "*"
-        elif event.widget == self.divButton:
-            self.cache += "/"
-        elif event.widget == self.modButton:
-            self.cache += "%"
-        elif event.widget == self.backButton:
+        # 取出事件发生的按钮对应的符号或数字
+        symbol = event.widget["text"]
+        # 控制和计算
+        if symbol == "<-":
             if self.cache:
                 self.cache = self.cache[:-1]
-        elif event.widget == self.acButton:
+        elif symbol == "AC":
             self.cache = ""
-        elif event.widget == self.equalButton:
+        elif symbol == "=":
+            # 直接使用eval函数计算
             self.cache = str(eval(self.cache))
+        else:
+            self.cache += symbol
+        # 显示出来
+        getattr(self, "output").set(self.cache)
 
-        self.output.set(self.cache)
 
-
-app = Window()
+app = Window("UI.json")
 # 设置窗口标题:
-app.master.title('计算器')
+app.master.title("计算器")
 # 主消息循环:
 app.mainloop()
--- idea-365/D029_JSON2GUI/json2gui.py
+++ idea-365/D029_JSON2GUI/json2gui.py
@@ -1,7 +1,6 @@
 import json
 import codecs
 import tkinter as tk
-import tkinter.ttk as ttk
 
 
 def read_json_file(json_file):
@@ -14,39 +13,41 @@ def read_json_file(json_file):
 def create_ui(self, json_file):
     widget = read_json_file(json_file)
 
-    if widget:
-        for k in widget:
-            # "class"和"grid"参数不存在直接弹出异常
-            if not widget[k].get("class"):
-                raise NameError("Parameter loss: class")
+    if not widget:
+        raise NameError("JSON is empty")
 
-            if not widget[k].get("grid"):
-                raise NameError("Parameter loss: grid")
+    for k in widget:
+        # "class"和"grid"参数不存在直接弹出异常
+        if not widget[k].get("class"):
+            raise NameError("Parameter loss: class")
 
-            widget_class = widget[k]["class"]
-            widget_grid = widget[k]["grid"]
-            widget_str_parm = widget[k]["string"] if widget[k].get("string") else {}
-            widget_int_parm = widget[k]["int"] if widget[k].get("int") else {}
-            widget_var = widget[k]["var"] if widget[k].get("var") else None
+        if not widget[k].get("grid"):
+            raise NameError("Parameter loss: grid")
 
-            # 动态生成控件，并添加字符串类型的参数（若有）
-            self.__dict__[k] = tk.__dict__[widget_class](self, **widget_str_parm)
-            # 为每个控件添加数值类型的参数（若有）
-            for pk in widget_int_parm:
-                self.__dict__[k][pk] = int(widget_int_parm[pk])
-            # 为每个控件绑定变量（若有）
-            if widget_var:
-                self.__dict__[widget_var] = tk.StringVar()
-                self.__dict__[k]["textvariable"] = self.__dict__[widget_var]
-            # 使用grid布局控件
-            self.__dict__[k].grid(**widget_grid)
+        widget_class = widget[k]["class"]
+        widget_grid = widget[k]["grid"]
+        widget_str_parm = widget[k]["string"] if widget[k].get("string") else {}
+        widget_int_parm = widget[k]["int"] if widget[k].get("int") else {}
+        widget_var = widget[k]["var"] if widget[k].get("var") else None
 
-
-# 绑定控件的指令
-def create_command(self, widget, command):
-    self.__dict__[widget]["command"] = command
+        # 动态生成控件，并添加字符串类型的参数（若有）
+        self.__dict__[k] = tk.__dict__[widget_class](self, **widget_str_parm)
+        # 为每个控件添加数值类型的参数（若有）
+        for pk in widget_int_parm:
+            self.__dict__[k][pk] = int(widget_int_parm[pk])
+        # 为每个控件绑定变量（若有）
+        if widget_var:
+            self.__dict__[widget_var] = tk.StringVar()
+            self.__dict__[k]["textvariable"] = self.__dict__[widget_var]
+        # 使用grid布局控件
+        self.__dict__[k].grid(**widget_grid)
 
 
-# 绑定事件
-def create_bind(self, widget, event_handle):
-    self.__dict__[widget].bind("<Button-1>", event_handle)
+# 通过json文件给控件绑定事件（若有）
+def create_all_binds(self, json_file):
+    widget = read_json_file(json_file)
+    for k in widget:
+        if widget[k].get("bind"):
+            for w in widget[k]["bind"]:
+                # 通过绑定方法的名称，来动态绑定给控件绑定事件
+                self.__dict__[k].bind(w, getattr(self, widget[k]["bind"][w]))

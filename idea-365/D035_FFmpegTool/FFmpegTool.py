dd6988f8892419cf7d4d513a9476210c8af25a4e
cforth <cforth@163.com>
2018-09-07 08:49:11 +0800

    增加保存设置的功能

--- idea-365/D035_FFmpegTool/FFmpegTool.py
+++ idea-365/D035_FFmpegTool/FFmpegTool.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import json
 import tkinter.filedialog as filedialog
 from json2gui import *
 
@@ -34,8 +35,10 @@ class Window(ttk.Frame):
         self.master.rowconfigure(0, weight=1)
         self.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.E, tk.W))
         self.columnconfigure(1, weight=1)
+        self.read_config()
 
     def intercept_video_button_callback(self, event=None):
+        splitext = os.path.splitext(self.current_video_path.get())[1]
         video_start_time = self.video_start_hour_time.get() + \
                            ":" + self.video_start_minute_time.get() + \
                            ":" + self.video_start_second_time.get()
@@ -43,7 +46,7 @@ class Window(ttk.Frame):
                          ":" + self.intercept_minute_time.get() + \
                          ":" + self.intercept_second_time.get()
         intercept_video(self.ffmpeg_path.get(), self.current_video_path.get(), video_start_time, intercept_time,
-                        os.path.join(self.video_save_dir_path.get(), self.video_save_name.get()) + ".mp4")
+                        os.path.join(self.video_save_dir_path.get(), self.video_save_name.get()) + splitext)
 
     def screen_shot_button_callback(self, event=None):
         video_start_time = self.video_start_hour_time.get() + \
@@ -52,10 +55,23 @@ class Window(ttk.Frame):
         screen_shot(self.ffmpeg_path.get(), self.current_video_path.get(), video_start_time,
                     os.path.join(self.video_save_dir_path.get(), self.video_save_name.get()) + ".png")
 
+    def save_config(self, config_dir):
+        with open("FFmpegConfig.json", "w") as f:
+            json.dump(config_dir, f)
+
+    def read_config(self):
+        with open("FFmpegConfig.json", "r") as f:
+            config_dir = json.load(f)
+            if config_dir and "ffmpeg_path" in config_dir:
+                self.ffmpeg_path.set(config_dir["ffmpeg_path"])
+
     def ffmpeg_from_button_callback(self, event=None):
         ffmpeg_path = filedialog.askopenfilename()
         if ffmpeg_path:
             self.ffmpeg_path.set(ffmpeg_path)
+            config_dir = dict()
+            config_dir["ffmpeg_path"] = ffmpeg_path
+            self.save_config(config_dir)
 
     def file_from_button_callback(self, event=None):
         video_path = filedialog.askopenfilename()

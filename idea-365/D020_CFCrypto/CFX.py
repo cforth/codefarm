33d015532a7166eeeea0864fbfa02513cf9e7685
cforth <cforth@163.com>
2018-11-07 10:35:37 +0800

    重构加密解密类

--- idea-365/D020_CFCrypto/CFX.py
+++ idea-365/D020_CFCrypto/CFX.py
@@ -5,130 +5,136 @@ import os
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
-
-# b'\0'填充密码
-def null_pad(data_to_pad):
-    data_len = len(data_to_pad)
-    # 不超过128位的密码填充长度（字节单位）
-    if data_len <= 16:
-        key_len = 16
-    # 128位以上，不超过192位的密码填充长度（字节单位）
-    elif 16 < data_len <= 24:
-        key_len = 24
-    # 192位以上，不超过256位的密码填充长度（字节单位）
-    elif 24 < data_len <= 32:
-        key_len = 32
-    # 超过256位的密码直接截断到256位长度
-    else:
-        return data_to_pad[:32]
-    # 进行'\0'填充
-    padding_len = key_len - len(data_to_pad)
-    padding = b'\0' * padding_len
-    return data_to_pad + padding
+# 读写文件时，每次读写的数据量
+BUFFER_SIZE = 10 * 1024 * 1024
 
 
-# 生成密钥
-def gen_aes_key(password, salt, use_md5):
-    # 将密码加盐，防止泄露原始密码
-    password = password + salt if salt else password
-    # use_md5为True值时，将密码转为md5值作为密钥，否则使用b'\0'填充密码
-    if use_md5:
-        md5 = hashlib.md5()
-        md5.update(password.encode('utf-8'))
-        key = md5.digest()
-    else:
-        key = null_pad(password.encode('utf-8'))
+# 加密解密基类，设置密码和其他参数
+class BaseCrypto(object):
+    def __init__(self, password, iv_str=None, salt=None, use_md5=False, use_urlsafe=False, buffer_size=BUFFER_SIZE):
+        self._iv_str = iv_str
+        # 生成密钥时，选择是否加盐，是否使用md5值
+        self.key = self.gen_aes_key(password, salt, use_md5)
+        self.use_urlsafe = use_urlsafe
+        self.buffer_size = buffer_size
+        self.cipher = None
 
-    return key
+    # 获得iv_str，初始化向量的字符串表现形式
+    @property
+    def iv_str(self):
+        return self._iv_str
+
+    # b'\0'填充密码
+    def null_pad(self, data_to_pad):
+        data_len = len(data_to_pad)
+        # 不超过128位的密码填充长度（字节单位）
+        if data_len <= 16:
+            key_len = 16
+        # 128位以上，不超过192位的密码填充长度（字节单位）
+        elif 16 < data_len <= 24:
+            key_len = 24
+        # 192位以上，不超过256位的密码填充长度（字节单位）
+        elif 24 < data_len <= 32:
+            key_len = 32
+        # 超过256位的密码直接截断到256位长度
+        else:
+            return data_to_pad[:32]
+        # 进行'\0'填充
+        padding_len = key_len - len(data_to_pad)
+        padding = b'\0' * padding_len
+        return data_to_pad + padding
+
+    # 生成密钥
+    def gen_aes_key(self, password, salt, use_md5):
+        # 将密码加盐，防止泄露原始密码
+        password = password + salt if salt else password
+        # use_md5为True值时，将密码转为md5值作为密钥，否则使用b'\0'填充密码
+        if use_md5:
+            md5 = hashlib.md5()
+            md5.update(password.encode('utf-8'))
+            key = md5.digest()
+        else:
+            key = self.null_pad(password.encode('utf-8'))
+
+        return key
 
 
 # 字符串加密解密类
-class StringCrypto(object):
-    def __init__(self, password, salt="", use_md5=False, use_urlsafe=False):
-        # 是否使用urlsafe模式下的base64编码
-        self.use_urlsafe = use_urlsafe
-        # 生成密钥时，选择是否加盐，是否使用md5值
-        self.key = gen_aes_key(password, salt, use_md5)
-        self.cipher = None
+class StringCrypto(BaseCrypto):
+    def __init__(self, password, iv_str=None, salt=None, use_md5=False, use_urlsafe=False):
+        super().__init__(password, iv_str, salt, use_md5, use_urlsafe)
 
     # 加密字符串，iv_str默认为None时随机生成
-    def encrypt(self, original_string, iv_str=None):
+    def encrypt(self, original_string):
         # 将原字符串长度补齐到AES.block_size的整数倍长度
         pad_byte_string = pad(original_string.encode('utf-8'), AES.block_size)
-        if iv_str:
+        if self._iv_str:
             # iv为初始化向量，AES为16字节
-            iv = base64.urlsafe_b64decode(iv_str) if self.use_urlsafe else base64.b64decode(iv_str)
+            iv = base64.urlsafe_b64decode(self._iv_str) if self.use_urlsafe else base64.b64decode(self._iv_str)
             self.cipher = AES.new(self.key, AES.MODE_CBC, iv)
         else:
-            # 未指定iv，则随机生成一个
+            # 未指定iv，则随机生成一个，并设定iv_str
             self.cipher = AES.new(self.key, AES.MODE_CBC)
             if self.use_urlsafe:
-                iv_str = base64.urlsafe_b64encode(self.cipher.iv).decode('utf-8')
+                self._iv_str = base64.urlsafe_b64encode(self.cipher.iv).decode('utf-8')
             else:
-                iv_str = base64.b64encode(self.cipher.iv).decode('utf-8')
+                self._iv_str = base64.b64encode(self.cipher.iv).decode('utf-8')
         # 使用AES-128的CBC模式加密字符串
         ct_bytes = self.cipher.encrypt(pad_byte_string)
         if self.use_urlsafe:
             encrypt_string = base64.urlsafe_b64encode(ct_bytes).decode('utf-8')
         else:
             encrypt_string = base64.b64encode(ct_bytes).decode('utf-8')
-        return iv_str, encrypt_string
+        return encrypt_string
 
     # 解密字符串
-    def decrypt(self, encrypt_string, iv_str):
+    def decrypt(self, encrypt_string):
         if self.use_urlsafe:
             encrypt_byte_string = base64.urlsafe_b64decode(bytes(map(ord, encrypt_string)))
-            iv = base64.urlsafe_b64decode(iv_str)
+            iv = base64.urlsafe_b64decode(self._iv_str)
         else:
             encrypt_byte_string = base64.b64decode(bytes(map(ord, encrypt_string)))
-            iv = base64.b64decode(iv_str)
+            iv = base64.b64decode(self._iv_str)
         # 使用AES-128的CBC模式进行解密
         self.cipher = AES.new(self.key, AES.MODE_CBC, iv)
         pad_byte_string = self.cipher.decrypt(encrypt_byte_string)
         original_string = unpad(pad_byte_string, AES.block_size).decode('utf-8')
-        return iv_str, original_string
+        return original_string
 
 
 # 将二进制数据加密或解密，返回二进制数据(一次性读入内存加密，用于小文件)
-class ByteCrypto(object):
-    def __init__(self, password, salt="", use_md5=False, use_urlsafe=False):
-        # 生成密钥时，选择是否加盐，是否使用md5值
-        self.key = gen_aes_key(password, salt, use_md5)
-        self.use_urlsafe = use_urlsafe
-        self.cipher = None
+class ByteCrypto(BaseCrypto):
+    def __init__(self, password, iv_str=None, salt=None, use_md5=False, use_urlsafe=False):
+        super().__init__(password, iv_str, salt, use_md5, use_urlsafe)
 
-    def encrypt(self, original_data, iv_str=None):
-        if iv_str:
+    def encrypt(self, original_data):
+        if self._iv_str:
             # iv为初始化向量，AES为16字节
-            iv = base64.urlsafe_b64decode(iv_str) if self.use_urlsafe else base64.b64decode(iv_str)
+            iv = base64.urlsafe_b64decode(self._iv_str) if self.use_urlsafe else base64.b64decode(self._iv_str)
             self.cipher = AES.new(self.key, AES.MODE_CBC, iv)
         else:
             # 未指定iv，则随机生成一个
             self.cipher = AES.new(self.key, AES.MODE_CBC)
             if self.use_urlsafe:
-                iv_str = base64.urlsafe_b64encode(self.cipher.iv).decode('utf-8')
+                self._iv_str = base64.urlsafe_b64encode(self.cipher.iv).decode('utf-8')
             else:
-                iv_str = base64.b64encode(self.cipher.iv).decode('utf-8')
-        return iv_str, self.cipher.encrypt(pad(original_data, AES.block_size))
+                self._iv_str = base64.b64encode(self.cipher.iv).decode('utf-8')
+        return self.cipher.encrypt(pad(original_data, AES.block_size))
 
-    def decrypt(self, data_to_decrypt, iv_str):
+    def decrypt(self, data_to_decrypt):
         if self.use_urlsafe:
-            iv = base64.urlsafe_b64decode(iv_str)
+            iv = base64.urlsafe_b64decode(self._iv_str)
         else:
-            iv = base64.b64decode(iv_str)
+            iv = base64.b64decode(self._iv_str)
         # 使用AES-128的CBC模式进行解密
         self.cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return iv_str, unpad(self.cipher.decrypt(data_to_decrypt), AES.block_size)
+        return unpad(self.cipher.decrypt(data_to_decrypt), AES.block_size)
 
 
 # 将文件加密或解密，指定block_size作为每次读取写入的数据量，用于大文件
-class FileCrypto(object):
-    def __init__(self, password, salt="", use_md5=False, use_urlsafe=False, block_size=10 * 1024 * 1024):
-        # 生成密钥时，选择是否加盐，是否使用md5值
-        self.key = gen_aes_key(password, salt, use_md5)
-        self.use_urlsafe = use_urlsafe
-        self.block_size = block_size
-        self.cipher = None
+class FileCrypto(BaseCrypto):
+    def __init__(self, password, iv_str=None, salt=None, use_md5=False, use_urlsafe=False, buffer_size=BUFFER_SIZE):
+        super().__init__(password, iv_str, salt, use_md5, use_urlsafe, buffer_size)
         # 加密解密的状态
         self.crypto_status = False
         # 已经读取的数据长度
@@ -162,7 +168,7 @@ class FileCrypto(object):
         try:
             with open(file_path, 'rb') as f:
                 self.read_len = 0
-                data_iter = iter(partial(f.read, self.block_size), b'')
+                data_iter = iter(partial(f.read, self.buffer_size), b'')
                 for data in data_iter:
                     if not self.crypto_status:
                         break
@@ -178,30 +184,29 @@ class FileCrypto(object):
         finally:
             self.crypto_status = False
 
-    def encrypt(self, file_path, output_file_path, iv_str=None):
-        if iv_str:
+    def encrypt(self, file_path, output_file_path):
+        if self._iv_str:
             # iv为初始化向量，AES为16字节
-            iv = base64.urlsafe_b64decode(iv_str) if self.use_urlsafe else base64.b64decode(iv_str)
+            iv = base64.urlsafe_b64decode(self._iv_str) if self.use_urlsafe else base64.b64decode(self._iv_str)
             self.cipher = AES.new(self.key, AES.MODE_CBC, iv)
         else:
             # 未指定iv，则随机生成一个
             self.cipher = AES.new(self.key, AES.MODE_CBC)
             if self.use_urlsafe:
-                iv_str = base64.urlsafe_b64encode(self.cipher.iv).decode('utf-8')
+                self._iv_str = base64.urlsafe_b64encode(self.cipher.iv).decode('utf-8')
             else:
-                iv_str = base64.b64encode(self.cipher.iv).decode('utf-8')
+                self._iv_str = base64.b64encode(self.cipher.iv).decode('utf-8')
 
         data_handle_func = self.cipher.encrypt
         # 读取到文件尾部时，执行尾部补位操作后加密
         data_end_handle_func = lambda d: self.cipher.encrypt(pad(d, AES.block_size))
         self.handle(file_path, output_file_path, data_handle_func, data_end_handle_func)
-        return iv_str
 
-    def decrypt(self, file_path, output_file_path, iv_str):
+    def decrypt(self, file_path, output_file_path):
         if self.use_urlsafe:
-            iv = base64.urlsafe_b64decode(iv_str)
+            iv = base64.urlsafe_b64decode(self._iv_str)
         else:
-            iv = base64.b64decode(iv_str)
+            iv = base64.b64decode(self._iv_str)
 
         # 使用AES-128的CBC模式进行解密
         self.cipher = AES.new(self.key, AES.MODE_CBC, iv)
@@ -209,4 +214,3 @@ class FileCrypto(object):
         # 读取到文件尾部时，执行解密后尾部去除补位
         data_end_handle_func = lambda d: unpad(self.cipher.decrypt(d), AES.block_size)
         self.handle(file_path, output_file_path, data_handle_func, data_end_handle_func)
-        return iv_str
--- idea-365/D020_CFCrypto/test_cfx.py
+++ idea-365/D020_CFCrypto/test_cfx.py
@@ -9,39 +9,40 @@ class TestCrypto(unittest.TestCase):
     def test_StringCrypto(self):
         iv_str = "UEr9si9EynusD5GGVuiqKw=="
         original_string = 'this is a secret!!! 这是一个秘密！！！'
-        my_cipher = StringCrypto('this is password')
-        iv_str, encrypt_str = my_cipher.encrypt(original_string, iv_str)
-        iv_str, decrypt_str = my_cipher.decrypt(encrypt_str, iv_str)
+        my_cipher = StringCrypto('this is password', iv_str)
+        encrypt_str = my_cipher.encrypt(original_string)
+        decrypt_str = my_cipher.decrypt(encrypt_str)
+        print(my_cipher.iv_str)
         self.assertEqual(encrypt_str, 'p27dj7RbfYbv1R8+NJJmQ5cjHacSKCBYAWa0Mx1QASUpA2BRxja0VelGCC33VS36')
         self.assertEqual(original_string, decrypt_str)
-        my_cipher = StringCrypto('this is password', use_md5=True)
-        iv_str, encrypt_str = my_cipher.encrypt(original_string, iv_str)
-        iv_str, decrypt_str = my_cipher.decrypt(encrypt_str, iv_str)
+        my_cipher = StringCrypto('this is password', iv_str, use_md5=True)
+        encrypt_str = my_cipher.encrypt(original_string)
+        decrypt_str = my_cipher.decrypt(encrypt_str)
         self.assertEqual(encrypt_str, 'Zd4q3Fj/rwMGJIxNBUYMttGQM9hr8QgKGrQ4KZz1G2VtI4B67I5pk3/mw8GtPyX0')
         self.assertEqual(original_string, decrypt_str)
-        my_cipher = StringCrypto('this is password', use_md5=True, use_urlsafe=True)
-        iv_str, encrypt_str = my_cipher.encrypt(original_string, iv_str)
-        iv_str, decrypt_str = my_cipher.decrypt(encrypt_str, iv_str)
+        my_cipher = StringCrypto('this is password', iv_str, use_md5=True, use_urlsafe=True)
+        encrypt_str = my_cipher.encrypt(original_string)
+        decrypt_str = my_cipher.decrypt(encrypt_str)
         self.assertEqual(original_string, decrypt_str)
         self.assertEqual(encrypt_str, 'Zd4q3Fj_rwMGJIxNBUYMttGQM9hr8QgKGrQ4KZz1G2VtI4B67I5pk3_mw8GtPyX0')
-        my_cipher = StringCrypto('this is password', use_md5=True, use_urlsafe=True)
-        iv_str, encrypt_str = my_cipher.encrypt(original_string)
+        my_cipher = StringCrypto('this is password', iv_str, use_md5=True, use_urlsafe=True)
+        encrypt_str = my_cipher.encrypt(original_string)
         print(iv_str)
         print(encrypt_str)
-        iv_str, decrypt_str = my_cipher.decrypt(encrypt_str, iv_str)
+        decrypt_str = my_cipher.decrypt(encrypt_str)
         self.assertEqual(original_string, decrypt_str)
 
     def test_ByteCrypto(self):
         my_cipher = ByteCrypto('this is very long password to test file crypto')
         with open('./testdata/test.png', 'rb') as f_from:
-            iv_str, encrypt_data = my_cipher.encrypt(f_from.read())
-            print(iv_str)
+            encrypt_data = my_cipher.encrypt(f_from.read())
+            print(my_cipher.iv_str)
             with open('./testdata/test.png.aes', 'wb') as f_to:
                 f_to.write(encrypt_data)
 
         with open('./testdata/test.png.aes', 'rb') as f_from:
-            iv_str, decrypt_data = my_cipher.decrypt(f_from.read(), iv_str)
-            print(iv_str)
+            decrypt_data = my_cipher.decrypt(f_from.read())
+            print(my_cipher.iv_str)
             with open('./testdata/aes_test.png', 'wb') as f_to:
                 f_to.write(decrypt_data)
 
@@ -51,9 +52,17 @@ class TestCrypto(unittest.TestCase):
 
     def test_FileCrypto(self):
         my_cipher = FileCrypto('this is very long password to test file crypto')
-        iv_str = my_cipher.encrypt('./testdata/test.pdf', './testdata/test.pdf.aes')
-        print(iv_str)
-        my_cipher.decrypt('./testdata/test.pdf.aes', './testdata/aes_test.pdf', iv_str)
+        my_cipher.encrypt('./testdata/test.pdf', './testdata/test.pdf.aes')
+        print(my_cipher.iv_str)
+        my_cipher.decrypt('./testdata/test.pdf.aes', './testdata/aes_test.pdf')
+        self.assertTrue(filecmp.cmp('./testdata/test.pdf', './testdata/aes_test.pdf'))
+        os.remove('./testdata/aes_test.pdf')
+        os.remove('./testdata/test.pdf.aes')
+        iv_str = "UEr9si9EynusD5GGVuiqKw=="
+        my_cipher = FileCrypto('this is very long password to test file crypto', iv_str)
+        my_cipher.encrypt('./testdata/test.pdf', './testdata/test.pdf.aes')
+        print(my_cipher.iv_str)
+        my_cipher.decrypt('./testdata/test.pdf.aes', './testdata/aes_test.pdf')
         self.assertTrue(filecmp.cmp('./testdata/test.pdf', './testdata/aes_test.pdf'))
         os.remove('./testdata/aes_test.pdf')
         os.remove('./testdata/test.pdf.aes')
@@ -61,10 +70,11 @@ class TestCrypto(unittest.TestCase):
     def test_BigFileCrypto(self):
         # 使用AES加密解密的演示,指定iv_str
         iv_str = "UEr9si9EynusD5GGVuiqKw=="
-        my_cipher = FileCrypto('this is very long password to test file crypto')
-        iv_str = my_cipher.encrypt('./testdata/test.mp4', './testdata/test.mp4.aes', iv_str)
-        print(iv_str)
-        my_cipher.decrypt('./testdata/test.mp4.aes', './testdata/aes_test.mp4', iv_str)
+        password = 'this is very long password to test file crypto'
+        my_cipher = FileCrypto(password, iv_str)
+        my_cipher.encrypt('./testdata/test.mp4', './testdata/test.mp4.aes')
+        print(my_cipher.iv_str)
+        my_cipher.decrypt('./testdata/test.mp4.aes', './testdata/aes_test.mp4')
         self.assertTrue(filecmp.cmp('./testdata/test.mp4', './testdata/aes_test.mp4'))
         os.remove('./testdata/aes_test.mp4')
         os.remove('./testdata/test.mp4.aes')
